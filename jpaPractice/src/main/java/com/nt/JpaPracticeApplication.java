ce39bfeeef97a50b721dbc081b6c120b356377de
vinny <ilovebluesky11@gmail.com>
2021-12-28 13:53:00 +0530

    some new changes ar ther in this code in the main class

--- jpaPractice/src/main/java/com/nt/JpaPracticeApplication.java
+++ jpaPractice/src/main/java/com/nt/JpaPracticeApplication.java
@@ -13,12 +13,18 @@ public class JpaPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaPracticeApplication.class, args);
+		
+		System.out.println("welcome to this page");
 	}
 	
 	
 	String url = "www.google.com" ; 
 	RestTemplate r  =  new RestTemplate() ; 
 	ResponseEntity<String>  re =  r.getForEntity(url, String.class) ; 
+	
+	
+	// e see buddy whether these new changes have been working fine or not
+	
 
 		
 	
